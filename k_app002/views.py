533373b40117b4df2190146f6ae00831376814bc
Katuhito Hara <katuhitohara@gmail.com>
2020-08-18 14:53:21 +0900

    QuerySet

--- k_app002/views.py
+++ k_app002/views.py
@@ -3,14 +3,21 @@ from django.http import HttpResponse
 # from django.views.generic import TemplateView
 # from k_app002.form import HelloForm
 from .models import Friend
-# from .forms import HelloForm
+from django.db.models import QuerySet
 
-def index(request):
-    num = Friend.objects.all().count()
-    first = Friend.objects.all().first()
-    last = Friend.objects.all().last()
-    data = [num, first, last]
-    # data = Friend.objects.all().values('id', 'name', 'age')
+def __new_str__(self):
+    result = ''
+    for item in self:
+        result += '<tr>'
+        for k in item:
+            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
+        result += '</tr>'
+    return result
+
+QuerySet.__str__ = __new_str__
+
+def index(request):   
+    data = Friend.objects.all().values('id', 'name', 'age')
     params = {
         'title': 'Hello',
         'data': data,
