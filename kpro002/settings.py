e5c46b36dd88ffa10154808e6607858551eedfb8
Katuhito Hara <katuhitohara@gmail.com>
2020-09-14 14:14:01 +0900

    sns second config and tests

--- kpro002/settings.py
+++ kpro002/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'k_app002',
-    # 'sns',
+    'sns',
 ]
 
 MIDDLEWARE = [
--- kpro002/urls.py
+++ kpro002/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('k_app002/', include('k_app002.urls')),
-    # path('sns/', include('sns.urls')),
+    path('sns/', include('sns.urls')),
 ]
